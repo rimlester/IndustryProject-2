b8ab4fd545202af65cc1c2c90d7673e0743b92ba
Rupert Lester <rupert@edocx.com.au>
2015-05-11 12:28:39 +1000

    Add dummy file creation
    reeeeeeee

--- my-app/src/main/java/com/uni/convert/App.java
+++ my-app/src/main/java/com/uni/convert/App.java
@@ -1,13 +1,32 @@
 package com.uni.convert;
 
+import java.io.File;
+import java.io.PrintWriter;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
+    	
+    	if(args.length == 0){
+    		throw new Exception("Not enough arguments");
+    	}
         System.out.println( "Hello World!" );
+    }
+    
+    
+    private static void WriteFile(String thing){
+    	try{
+    		PrintWriter p = new PrintWriter(thing + ".out.rdf"); 
+    		p.write("Todo");
+    		p.close();
+    	}catch(Exception ex){
+    		//fuckoff reeeeeeeeeeeee
+    	}
+    	
     }
 }
